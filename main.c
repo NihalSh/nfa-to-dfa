223fba333983c7f08c655903bd330889bc62a0ad
NihalSh <nihal.sh.1995@gmail.com>
2017-04-01 18:11:43 +0530

    matrix initialization

--- main.c
+++ main.c
@@ -15,20 +15,35 @@ int getInput()
 	int numStates;
 	int i;
 	int j;
+	int k;
+	int *** matrix = NULL;
 	printf("Number of Input Symbols: ");
 	scanf(" %d", &numSym);
 	printf("Number of States: ", &numSym);
 	scanf(" %d", &numStates);
-	if ((numSym > 26) || (numSym < 1) || (numStates > 26) || (numStates < 1)) {
+	if ((numSym < 1) || (numStates < 1)) {
 		return 1;
 	}
+	
+	//initialize matrix
+	matrix = (int ***) malloc(numStates*sizeof(int **));
+	for (i = 0; i < numStates; i++) {
+		matrix[i] = (int **) malloc(numSym*sizeof(int *));
+		for (j = 0; j < numSym; j++) {
+			matrix[i][j] = (int *) malloc(numStates*sizeof(int));
+			for (k = 0; k < numStates; k++) {
+				matrix[i][j][k] = 0;
+			}
+		}
+	}
+
 	printf("\tepsilon");
-	for (i = 0; i < numSym; i++) {
-		printf("\t%c", 'a' + i);
+	for (i = 1; i <= numSym; i++) {
+		printf("\t%d", i);
 	}
 	printf("\n");
 	for (i = 0; i < numStates; i++) {
-		printf("%c", 'A' + i);
+		printf("q%d", i);
 		for (j = 0; j < numSym; j++) {
 			printf("\t");
 		}
