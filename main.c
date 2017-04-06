5d7c0f7a8345b798f099676ea99dc4237718f173
NihalSh <nihal.sh.1995@gmail.com>
2017-04-06 19:20:05 +0530

    compute DFA

--- main.c
+++ main.c
@@ -17,24 +17,61 @@ int main()
 	int numSymbols;
 	int ** dStates = NULL;
 	int ** dMatrix = NULL;
+	int sizeDFA = 0;
 	getInput(&matrix, &numStates, &numSymbols);
 	display(matrix, numStates, numSymbols);
-	compute(&dStates, &dMatrix, matrix, numStates, numSymbols);
+	compute(&dStates, &dMatrix, matrix, numStates, numSymbols, &sizeDFA);
+	displayDFA(dStates, dMatrix, numStates, numSymbols, sizeDFA);
+	return 0;
+}
+int displayDFA(int ** dStates, int ** dMatrix, int numStates, int numSymbols, int sizeDFA)
+{
+	int i;
+	int j;
+	int k;
+	int stateId = -1;
+	printf("\nDFA\n");
+
+	for (i = 1; i < numSymbols; i++) {
+		printf("\t%d", i);
+	}
+	printf("\n");
+	for(i = 0; i < sizeDFA; i++) {
+		displayDfaState(dStates[i], numStates);
+		for(j = 1; j < numSymbols; j++) {
+			printf("\t");
+			stateId = dMatrix[i][j];
+			displayDfaState(dStates[i], numStates);
+		}
+		printf("\n");
+	}
 	return 0;
 }
 
-int compute(int *** dStates, int *** dMatrix, int *** matrix, int numStates, int numSymbols)
+int displayDfaState(int * state, int numStates)
+{
+	int j;
+	printf("{ ");
+	for(j = 0; j < numStates; j++) {
+		if (state[j] == 1) {
+			printf("q%d ", j);
+		}
+	}
+	printf("}");
+}
+
+int compute(int *** dStates, int *** dMatrix, int *** matrix, int numStates, int numSymbols, int * sizeDFA)
 {
 	int j;
 	int i = 0;
 	int size = 1;
+	int * newState = NULL;
+	int index = -1;
 	//q0 is the start state;
 
 	//initialize a state array
-	int * state = (int *) malloc(numStates * sizeof(int));
-	for (j = 0; j < numStates; j++) {
-		state[j] = 0;
-	}
+	int * state = NULL;
+	initialize_stateArray(&state, numStates);
 
 	//find epsilon closure of the start state
 	state[0] = 1;
@@ -44,11 +81,70 @@ int compute(int *** dStates, int *** dMatrix, int *** matrix, int numStates, int
 	dStates[0][size - 1] = state;
 
 	while(i < size) {
-		for (j = 0; j < numStates; j++) {
+		for (j = 1; j < numSymbols; j++) {
+			initialize_stateArray(&newState, numStates);
+			move(matrix, dStates[0][i], j, newState, numStates);
+			e_close(matrix, numSymbols, numStates, newState);
+			//if newState not in dStates => size++ , push newState to dStates
+			dMatrix[0] = (int **) realloc(dMatrix[0], (i + 1) * sizeof(int *));
+			dMatrix[0][i] = (int *) malloc(numSymbols * sizeof(int));
 
+			index = locate(*dStates, size, newState, numStates);
+			if (index == -1) {
+				size++;
+				dStates[0] = (int **) realloc(dStates[0], size * sizeof(int *));
+				dStates[0][size - 1] = newState;
+				index = size - 1;
+			}
+			dMatrix[0][i][j] = index;
+			//dMatrix reallocate and set dMatrix[i][j] = index(newState, dStates)
 		}
 		i++;
 	}
+	sizeDFA[0] = size;
+	return 0;
+}
+
+int locate(int ** dStates, int size, int * state, int numStates)
+{
+	int i;
+	int j;
+	for (i = 0; i < size; i++) {
+		for (j = 0; j < numStates; j++) {
+			if (state[j] != dStates[i][j]) {
+				break;
+			}
+		}
+		if (j == numStates) {
+			return i;
+		}
+	}
+	return -1;
+}
+int initialize_stateArray(int ** state, int numStates)
+{
+	int j;
+	state[0] = (int *) malloc(numStates * sizeof(int));
+	for (j = 0; j < numStates; j++) {
+		state[0][j] = 0;
+	}
+	return 0;
+}
+
+int move(int *** matrix, int * state, int symbol, int * newState, int numStates)
+{
+	int i;
+	int j;
+	for (i = 0; i < numStates; i++) {
+		if (state[i] == 1) {
+			for (j = 0; j < numStates; j++) {
+				if (matrix[i][symbol][j] == 1) {
+					newState[j] = 1;
+				}
+			}
+		}
+	}
+	return 0;
 }
 
 int display(int *** matrix, int numStates, int numSymbols)
